fa9e3d373b1d23f739837f6e01ee155670e889bb
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-10-18 09:31:17 +0200

    --allow-empty-message

--- lib/uploadfile.ts
+++ lib/uploadfile.ts
@@ -6,8 +6,43 @@ export function uploadFiles(file: File, dbReference: any) {
   const reader = new FileReader();
 
   reader.readAsText(file);
-  const fileContent = JSON.stringify(reader.result as string);
-  console.log(reader.result);
+  console.log(reader.result)
+  const fileContent = JSON.stringify(reader.result);
+      console.log(fileContent);
+  
+  // reader.onload = async () => {
+  //   try {
+  //     
 
-    
+  // //     if (Array.isArray(fileContent)) {
+  // //       for (const item of fileContent) {
+  // //         await prisma.tikTokAccount.create({
+  // //           data: {
+  // //             tiktokId: item.tiktokId,
+  // //             authorId: item.authorId,
+  // //             createTime: item.createTime,
+  // //             diggCount: item.diggCount,
+  // //             playCount: item.playCount,
+  // //             uniqueId: item.uniqueId,
+  // //             nickname: item.nickname,
+  // //             followerCount: item.followerCount,
+  // //             heartCount: item.heartCount,
+  // //             videoCount: item.videoCount,
+  // //             description: item.itdescription,
+  // //             tags: item.tags,
+  // //             name: item.name,
+  // //             email: item.email,
+  // //             image: item.image
+  // //           },
+  // //         });
+  // //       }
+  // //       console.log('Data uploaded successfully.');
+  // //       console.log('File content:', reader.result);
+  // //     } else {
+  // //       console.error('The file does not contain a valid array of data.');
+  // //     }
+  // //   } catch (error) {
+  // //     console.error('Error processing file or saving to database:', error);
+  // //   }
+  // // };
 }
