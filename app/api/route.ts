cbf275eb08eb61b01d470dd1e9546edbe93fc4ac
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-10-19 14:44:24 +0200

    route POST works

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,1 +1,16 @@
-import {NextRe\}
+const content = require("@/lib/data");
+
+export async function GET(request: Request) {
+  return new Response("bsfasfasfsa");
+}
+
+
+export async function POST(request: Request) {
+  const bla = content
+  const body = request.json();
+  console.log(body);
+
+  return new Response(JSON.stringify(bla));
+
+
+}
