58ac43591ba5cde5ef42e9c2b838f670459a62c9
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-10-20 16:51:52 +0200

    lastlogut has been deleted from route ts [...nextauth] and prisma schema

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,10 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const handler = NextAuth({
-  
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
@@ -14,16 +13,16 @@ const handler = NextAuth({
   ],
   callbacks: {
     async signIn({ user, account }) {
-     const existingUser = await prisma.user.findUnique({
-    where: { email: user.email },
-    });
+      const existingUser = await prisma.user.findUnique({
+        where: { email: user.email },
+      });
       if (existingUser) {
         await prisma.user.update({
           where: { id: existingUser.id },
           data: {
             name: user.name,
             image: user.image,
-            updatedAt: new Date(),          
+            updatedAt: new Date(),
           },
         });
       } else {
@@ -32,28 +31,12 @@ const handler = NextAuth({
             name: user.name,
             email: user.email,
             image: user.image,
-            updatedAt: new Date()
+            updatedAt: new Date(),
           },
         });
       }
 
       return user;
-    },
-    async signOut({ user }) {
-      const existingUser = await prisma.user.findUnique({
-        where: { email: user.email },
-      });
-
-      if (existingUser && existingUser.lastLogout === null) {
-        await prisma.user.update({
-          where: { id: existingUser.id, lastLogin: null },
-          data: {
-            lastLogout: new Date(), // Aktualizuj lastLogout podczas wylogowywania
-          },
-        });
-      }
-
-      return null;
     },
   },
 });
